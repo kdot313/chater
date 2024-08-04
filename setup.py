fc112d7530c38791eb6767e213cfe681c47bf758
JATIN SAREEN <102468240+jatin-12-2002@users.noreply.github.com>
2024-08-04 16:13:14 +0000

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="E_commerce_ChatBot",
+    version="0.0.1",
+    author="Jatin",
+    author_email="sareenjatin002@gmail.com",
+    packages=find_packages(),
+    install_requires=['langchain-astradb','langchain ','langchain-openai','datasets','pypdf','python-dotenv','flask']
+)
